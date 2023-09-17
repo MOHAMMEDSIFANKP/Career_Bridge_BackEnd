0fd70d999380c6ae2cc55656ac749cc28b479ad7
sifan <sifan007sifu@gmail.com>
2023-09-17 16:10:44 +0530

    still pending in company profile

--- company/models.py
+++ company/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from api.models import User
 # Create your models here.
 
+
 class CompanyInfo(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=100)
-    industry = models.CharField(max_length=100,null=True)
-    company_size = models.CharField(max_length=100,null=True)
-    company_type = models.CharField(max_length=100,null=True)
-    gst = models.CharField(max_length=100,null=True)
+    industry = models.CharField(max_length=100, null=True)
+    company_size = models.CharField(max_length=100, null=True)
+    company_type = models.CharField(max_length=100, null=True)
+    gst = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
+    streetaddress = models.TextField(null=True)
+    contry = models.CharField(max_length=100,null=True)
+    state = models.CharField(max_length=100,null=True)
+    district = models.CharField(max_length=100,null=True)
+    zipcode = models.BigIntegerField(null=True)
     is_verify = models.BooleanField(default=False)
-
